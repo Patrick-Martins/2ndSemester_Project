45fd00241a568de873165928c44619fc4974d784
Mac <mac@Macs-MacBook-Air-2.local>
2019-12-13 12:57:04 +0100

    shop fetching

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -10,17 +11,23 @@ function init() {
     if (id) {
         getEventData();
     } else if (category) {
-        getCategoryData();
-
+        if (category == 5) {
+            getCategoryData();
+        } else {
+            showGallery();
+        }
     }
 }
+
+const filter = document.querySelector(".filterGallery");
+
 
 function getCategoryData() {
     const urlParams = new URLSearchParams(window.location.search);
     const category = urlParams.get("category");
 
 
-    fetch("http://pjmelite.dk/KEA_2Semester/2Sem_Project/wp_2ndSemProj/wp-json/wp/v2/painting?_embed&categories=" + category).then(res => res.json()).then(showStuff);
+    fetch("http://pjmelite.dk/KEA_2Semester/2Sem_Project/wp_2ndSemProj/wp-json/wp/v2/gallery_element?_embed&categories=" + category).then(res => res.json()).then(showStuff);
 }
 
 function showStuff(data) {
@@ -32,23 +39,43 @@ function showElements(element) {
     console.log(element);
     const urlParams = new URLSearchParams(window.location.search);
     const category = urlParams.get("category");
+    const template = document.querySelector(".galleryTemplate").content;
+    const templateCopy = template.cloneNode(true);
 
+    const elementContainer = templateCopy.querySelector(".type");
+    const imgPath = element._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+    elementContainer.style.backgroundImage = `url(${imgPath})`;
+    templateCopy.querySelector("h2").textContent = element.title.rendered;
+    elementContainer.addEventListener("click", function () {
+        location.href = `sub-gallery.html/category=${element.title.rendered}`
+        console.log(element.categories);
+        if (element.categories[0] === 5) {
+            console.log(element.categories[1]);
+            location.href = `sub-gallery.html?category=${element.categories[1]}`
+        } else {
+            console.log(element.categories[0]);
+            location.href = `sub-gallery.html?category=${element.categories[0]}`
+        }
+    })
 
-    if (category == 5) {
-        const template = document.querySelector(".galleryTemplate").content;
-        const templateCopy = template.cloneNode(true);
+    document.querySelector(".types").appendChild(templateCopy);
+}
 
-        const elementContainer = templateCopy.querySelector(".type");
-        const imgPath = element._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
-        elementContainer.style.backgroundImage = `url(${imgPath})`;
+function showGallery() {
+    console.log("showgallery");
+    const urlParams = new URLSearchParams(window.location.search);
+    const category = urlParams.get("category");
+    console.log(category);
+    fetch("http://pjmelite.dk/KEA_2Semester/2Sem_Project/wp_2ndSemProj/wp-json/wp/v2/painting?_embed&categories=" + category).then(res => res.json()).then(galleryForEach)
+}
 
-        elementContainer.addEventListener("click", function () {
-            window.location.href = "sub-gallery?category=" + element.id;
-        })
-        templateCopy.querySelector("h2").textContent = element.title.rendered;
+function galleryForEach(item) {
+    console.log(item);
+}
 
-        document.querySelector(".types").appendChild(templateCopy);
-    } else if (category == 7) {
+function showGalleryItem(img) {
+
+    if (category == 7) {
         const template = document.querySelector(".shopTemplate").content;
         const templateCopy = template.cloneNode(true);
 
@@ -59,9 +86,14 @@ function showElements(element) {
         templateCopy.querySelector(".shopElemPrice").textContent = element.price;
 
         document.querySelector(".shop-container").appendChild(templateCopy);
+    } else {
+
+        const template = document.querySelector("template").content;
+        const templateCopy = template.cloneNode(true);
+        const imgPath = img._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+        templateCopy.querySelector("img").setAttribute("src", imgPath);
+        document.querySelector(".gallery").appendChild(templateCopy);
     }
-
-
 }
 
 const hamburguerBTN = document.getElementById("hamburguer");
